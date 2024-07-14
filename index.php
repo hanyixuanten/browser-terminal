18d7b194df98dce089c570bfdb2e626b1f4dc5f6
hanyixuanten <hanyixuanten@outlook.com>
2024-07-14 16:45:29 +0800

    first ver:command help and clear

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Terminal</title>
+    <style>
+        body {
+            background-color: black;
+        }
+    </style>
+    <script>
+        function changeFrameHeight() {
+            var iframe = document.getElementById("if");
+            iframe.height = document.documentElement.clientHeight - 50;
+        }
+        window.onresize = function() {
+            changeFrameHeight();
+        }
+    </script>
+
+</head>
+
+<body>
+    <iframe src="./terminal.php" onload="changeFrameHeight()" height="" width="100%" scrolling="auto" frameborder="0" id="if" name="if"></iframe>
+    <form method="post" action="./terminal.php" target="if">
+        <input name="command" required />
+        <input type="submit" value="Submit" />
+    </form>
+</body>
+
+</html>
--- terminal.php
+++ terminal.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+<?php
+ini_set("display_errors", "Off");
+if (file_get_contents("recent.txt") == NULL) {
+    file_put_contents("recent.txt", "Welcome to the terminal!<br/>Type 'help' to see a list of commands.<br/>");
+}
+if (file_get_contents("pwd.txt") == NULL) {
+    file_put_contents("pwd.txt", "/");
+}
+?>
 <html>
 
 <head>
@@ -11,8 +20,22 @@
 
 <body>
     <?php
-    $output_str = "Welcome to the terminal!<br/>Type 'help' to see a list of commands.<br/>";
+    $pwd = file_get_contents("pwd.txt");
+    file_put_contents("recent.txt", $pwd . " $ " . $_POST['command'] . "<br/>", FILE_APPEND);
+    $output_str = file_get_contents("recent.txt");
     echo $output_str;
+    if ($_POST['command'] == NULL) {
+        echo "<script>window.scrollTo(0,document.body.scrollHeight)</script>";
+        exit(0);
+    } else if ($_POST['command'] == "clear") {
+        file_put_contents("recent.txt", "");
+    } else if ($_POST['command'] == "help") {
+        $help = file_get_contents("help.txt");
+        file_put_contents("recent.txt", $help, FILE_APPEND);
+    } else {
+        file_put_contents("recent.txt", "Unknown command.<br/>", FILE_APPEND);
+    }
+    echo "<script>window.location.href=\"terminal.php\";</script>";
     ?>
 </body>
 
