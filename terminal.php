5953d603ae04e6a30caf2cd769ffa08e6a0de286
hanyixuanten <hanyixuanten@outlook.com>
2024-07-14 20:15:18 +0800

    fix bug:when a dir is deleted, pwd is still in it

--- terminal.php
+++ terminal.php
@@ -7,6 +7,10 @@ if (file_get_contents("recent.txt") == NULL) {
 if (file_get_contents("pwd.txt") == NULL) {
     file_put_contents("pwd.txt", "/");
 }
+$pwd = file_get_contents("pwd.txt");
+if(!opendir("fileroot" . $pwd)){
+    !file_put_contents("pwd.txt", "/");
+}
 if (!opendir("fileroot")) {
     if (!mkdir("fileroot")) {
         echo "Error creating directory.No permissions?";
@@ -35,18 +39,34 @@ if (!opendir("fileroot")) {
     if ($commands[0] == NULL) {
         echo "<script>window.scrollTo(0,document.body.scrollHeight)</script>";
         exit(0);
-    } else if ($commands[0] == "pwd") {
+    }
+    if ($commands[0] == "pwd" & sizeof($commands) == 1) {
         file_put_contents("recent.txt", $pwd . "<br/>", FILE_APPEND);
     } else if ($commands[0] == "mkdir" && sizeof($commands) == 2) {
-        if (!opendir("fileroot/".$commands[1])) {
-            if (!mkdir("fileroot/".$commands[1])) {
+        if (!opendir("fileroot" . $pwd . "/" . $commands[1])) {
+            if (!mkdir("fileroot" . $pwd . "/" . $commands[1])) {
                 file_put_contents("recent.txt", "Error creating directory.No permissions?<br/>", FILE_APPEND);
             }
         } else {
             file_put_contents("recent.txt", "Directory already exists.<br/>", FILE_APPEND);
         }
-    } else if ($commands[0] == "clear") {
+    } else if ($commands[0] == "clear" & sizeof($commands) == 1) {
         file_put_contents("recent.txt", "");
+    } else if ($commands[0] == "cd" & sizeof($commands) == 2) {
+        if ($commands[1] == "..") {
+            $pwd = substr($pwd, 0, strrpos($pwd, "/"));
+            file_put_contents("pwd.txt", $pwd);
+        } else {
+            if (opendir("fileroot" . $pwd . "/" . $commands[1])) {
+                if ($pwd == "/")
+                    file_put_contents("pwd.txt", $pwd . $commands[1]);
+                else {
+                    file_put_contents("pwd.txt", $pwd . "/" . $commands[1]);
+                }
+            } else {
+                file_put_contents("recent.txt", "Directory does not exist.<br/>", FILE_APPEND);
+            }
+        }
     } else if ($commands[0] == "help") {
         $help = file_get_contents("static/terminal.php/help.txt");
         if (sizeof($commands) == 1) {
