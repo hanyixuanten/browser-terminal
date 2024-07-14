73da7784122f6df6861de5fdf91404a19988ce59
hanyixuanten <hanyixuanten@outlook.com>
2024-07-14 13:42:57 +0800

    Initial commit - creates new HTML, CSS, and PHP files for terminal emulator

---
+++ terminal.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <!-- <meta http-equiv="refresh" content="2"> -->
+    <title>frame</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+    <?php
+    $output_str = "Welcome to the terminal!<br/>Type 'help' to see a list of commands.<br/>";
+    echo $output_str;
+    ?>
+</body>
+
+</html>
