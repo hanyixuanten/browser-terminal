29b48a8e989cd5ed7c0abc6c2b2b2195db453561
hanyixuanten <hanyixuanten@outlook.com>
2024-07-14 20:23:13 +0800

    add help cd;fix bug:cd into a relative path

--- terminal.php
+++ terminal.php
@@ -8,7 +8,7 @@ if (file_get_contents("pwd.txt") == NULL) {
     file_put_contents("pwd.txt", "/");
 }
 $pwd = file_get_contents("pwd.txt");
-if(!opendir("fileroot" . $pwd)){
+if (!opendir("fileroot" . $pwd)) {
     !file_put_contents("pwd.txt", "/");
 }
 if (!opendir("fileroot")) {
@@ -52,10 +52,12 @@ if (!opendir("fileroot")) {
         }
     } else if ($commands[0] == "clear" & sizeof($commands) == 1) {
         file_put_contents("recent.txt", "");
-    } else if ($commands[0] == "cd" & sizeof($commands) == 2) {
-        if ($commands[1] == "..") {
+    } else if ($commands[0] == "cd") {
+        if ($commands[1] == ".." || sizeof($commands) == 1) {
             $pwd = substr($pwd, 0, strrpos($pwd, "/"));
             file_put_contents("pwd.txt", $pwd);
+        } else if (str_starts_with($commands[1], "/")) {
+            file_put_contents("pwd.txt", $commands[1]);
         } else {
             if (opendir("fileroot" . $pwd . "/" . $commands[1])) {
                 if ($pwd == "/")
